029f58d795a8b9f50fc937f673e86f0a77f12280
BOUZAR Mohamed Ali <bouzarmohamedali@gmail.com>
2026-07-07 23:02:49 +0100

    main.c

---
+++ 02-Variables/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+  float moy;
+  printf("Give a student grade average:");
+  scanf("%f",&moy);
+
+  printf("The student grade average given is:%.2f",moy);
+  return 0;
+}
