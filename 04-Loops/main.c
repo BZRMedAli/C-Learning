57cfb3e7a192d9efdd17b9f3f28e33b004f29034
BOUZAR Mohamed Ali <bouzarmohamedali@gmail.com>
2026-07-08 22:37:11 +0100

    main.c

---
+++ 04-Loops/main.c
@@ -0,0 +1,55 @@
+//For loop
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n,i;
+    printf("Give a number of repetitions you want:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+    printf("Hello, World! \n");    //We use \n to break a line in C
+    }
+    return 0;
+}	
+
+//While loop
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n,i;
+    printf("Give a number of repetitions you want:");
+    scanf("%d",&n);
+    i=1;    //We initialise i to 1 to start from 1 into n chosen by the user
+    while (i<=n){
+    printf("Hello, World! \n");
+    i++;
+    }
+
+    return 0;
+}
+
+//do...while loop
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int n,i;
+    printf("Give a number of repetitions you want:");
+    scanf("%d",&n);
+    i=1;    
+    do{
+    printf("Hello, World! \n");
+    i++;
+    }while (i<=n);  //Here we set the condition in the end
+
+    return 0;
+}
+
+
